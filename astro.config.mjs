842d6d9347aa1fb23c09c4c33bac6a736c459def
Paul Applegate <paul@theapplegates.com>
2022-12-13 22:18:30 +0000

    domain change

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://lucky-monstera-9fbbf3.netlify.app/",
+  site: "https://paper.paulapplegate.com/",
   integrations: [
     tailwind({
       config: {
